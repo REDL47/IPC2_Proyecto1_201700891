9e434c054888744a6f2773135683cc66ea24d652
REDL47 <Ronydeleon23@gmail.com>
2021-03-04 01:32:45 -0600

    Release1
    ProyectoFase_inicial

---
+++ Index.py
@@ -0,0 +1,40 @@
+
+
+
+
+def menu():
+    op = 0
+    while(op!=6):
+        print("1. Cargar Archivo de entrada")
+        print("2. Procesar Archivo")
+        print("3. Escribir Archivo de salida")
+        print("4. Mostrar datos del estudiante")
+        print("5. Generar grafica")
+        print("6. Salir")
+        op = int (input ("Ingrese Opcion: "))
+        ruta = ""
+        if op == 1:
+            ruta = input("Ingrese ruta del archivo:")
+
+
+        elif op==2:
+            print("Opción desplegar listas ordenadas:")
+            
+
+        elif op==3:
+            print("Opción desplegar búsquedas:")
+            
+
+        elif op==4:
+            print("Rony Emanuel De Leon Bulux")
+            print("201700891")
+            print("Introduccion a la Programación y Comutacion 2 Seccion C")
+            print("Ingenieria en Ciencias y Sistemas")
+            print("4to Semestre")
+            
+
+        elif op==5:
+            print("Opción Desplegar Todas a archivo")
+            
+print("Bienvenido")
+menu()
